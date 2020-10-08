fdd1a681e346caf2edb9d25e7d9f77ea527c98a5
kdflint <kathy.flint@northbridgetech.org>
2020-10-08 10:56:38 -0500

    placeholders

--- dashboard/src/main/java/com/kathyflint/lil/dashboard/manage/DashboardManager.java
+++ dashboard/src/main/java/com/kathyflint/lil/dashboard/manage/DashboardManager.java
@@ -46,6 +46,22 @@ public class DashboardManager {
     private void initializeTechSupport() {
         AppProperties props = (AppProperties) DashboardManager.context.getBean("appProperties");
         DashboardManager.dashboardStatusDao.setProperty("softwareBuild", props.getRuntimeProperties().getProperty("software.build", "unknown"));
+        // Make an AppSupportStatus payload
+        // AppSupportStatus status =
+        // Build a Message
+
+        /**
+         * Internally managed Headers
+         *                 ID	java.util.UUID
+         *                 TIMESTAMP	java.lang.Long
+         *                 EXPIRATION_DATE	java.lang.Long
+         *                 CORRELATION_ID	java.lang.Object
+         *                 REPLY_CHANNEL	java.lang.Object (can be a String or MessageChannel)
+         *                 ERROR_CHANNEL	java.lang.Object (can be a String or MessageChannel)
+         *                 SEQUENCE_NUMBER	java.lang.Integer
+         *                 SEQUENCE_SIZE	java.lang.Integer
+         *                 PRIORITY	MessagePriority (an enum)
+         */
     }
 
     private void initializeGridStatus() {
