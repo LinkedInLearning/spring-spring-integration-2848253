8bcc0aa10439731806f268e69cfabe90bfaaa267
kdflint <kathy.flint@northbridgetech.org>
2020-10-08 10:53:40 -0500

    placeholders

---
+++ dashboard/src/main/java/com/kathyflint/lil/dashboard/util/AppSupportStatus.java
@@ -0,0 +1,23 @@
+package com.kathyflint.lil.dashboard.util;
+
+import java.util.Date;
+
+public class AppSupportStatus {
+
+    private String snapVersion;
+
+    private Date snapTime;
+
+    public AppSupportStatus(String version, Date dttm) {
+        this.snapVersion = version;
+        this.snapTime = dttm;
+    }
+
+    public String getVersion() {
+        return snapVersion;
+    }
+
+    public Date getTime() {
+        return snapTime;
+    }
+}
