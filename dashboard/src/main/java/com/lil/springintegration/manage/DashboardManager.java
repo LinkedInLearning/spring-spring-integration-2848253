02f77b6c428c9c1f6bf8468754107a737b2a88ce
kdflint <kathy.flint@northbridgetech.org>
2020-10-08 12:25:58 -0500

    update build property

--- dashboard/src/main/java/com/lil/springintegration/manage/DashboardManager.java
+++ dashboard/src/main/java/com/lil/springintegration/manage/DashboardManager.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.messaging.support.MessageBuilder;
 
 import java.util.Date;
 import java.util.Properties;
@@ -38,23 +40,14 @@ public class DashboardManager {
     private void initializeTechSupport() {
         AppProperties props = (AppProperties) DashboardManager.context.getBean("appProperties");
         DashboardManager.dashboardStatusDao.setProperty("softwareBuild", props.getRuntimeProperties().getProperty("software.build", "unknown"));
+
         // Make an AppSupportStatus payload
         // AppSupportStatus status =
-        // AppSupportStatus status =
+
         // Build a Message
 
-        /**
-         * Internally managed Headers
-         *                 ID	java.util.UUID
-         *                 TIMESTAMP	java.lang.Long
-         *                 EXPIRATION_DATE	java.lang.Long
-         *                 CORRELATION_ID	java.lang.Object
-         *                 REPLY_CHANNEL	java.lang.Object (can be a String or MessageChannel)
-         *                 ERROR_CHANNEL	java.lang.Object (can be a String or MessageChannel)
-         *                 SEQUENCE_NUMBER	java.lang.Integer
-         *                 SEQUENCE_SIZE	java.lang.Integer
-         *                 PRIORITY	MessagePriority (an enum)
-         */
+        // To send our message, we need a channel!
+
     }
 
     private void initializeGridStatus() {
